cd0521dbd967d968300cfce1128d60f313ad8b70
Alex.Liu <larch.liu@gmail.com>
2022-10-06 14:22:11 +0800

    feat: monorepo init

---
+++ README.md
@@ -0,0 +1,9 @@
+<h1 align="center">
+Poto
+</h1>
+
+<p align="center">
+An open-source low-code Platform for Designers.
+<br>Make design simple.
+<br>
+<br>
---
+++ widget-plugins/template/src/index.ts
@@ -0,0 +1,10 @@
+const settings = {
+  name: 'plugin1',
+  type: 'widgetPlugin1',
+}
+
+export default {
+  widgetView: 'plugin1',
+  widgetSettingsView: 'widgetSettingsView1',
+  defaultSettings: settings,
+}
