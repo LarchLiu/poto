69d07066fcf0e354bea560a73ee1e494414a0bd9
Alex.Liu <larch.liu@gmail.com>
2022-10-10 22:28:47 +0800

    feat(core): utils of evaluate dynamic string

--- core/utils/src/index.ts
+++ core/utils/src/index.ts
@@ -1,5 +1,27 @@
 import { camelCase } from 'scule'
+import { v4 as uuidv4 } from 'uuid'
+import type { BlockItem } from '@poto/types'
+export * from './evaluateDynamicString'
 
 export function camelCaseWithoutAt(str: string) {
   return camelCase(str.replaceAll('@', ''))
+}
+
+export const UUID = () => {
+  return uuidv4()
+}
+
+const setID = (item: BlockItem) => {
+  if (item.options.list) {
+    item.options.list.map((i) => {
+      return setID(i)
+    })
+  }
+  item.id = UUID()
+  return item
+}
+
+export const cloneItem = (item: BlockItem) => {
+  const i: BlockItem = JSON.parse(JSON.stringify(item))
+  return setID(i)
 }
