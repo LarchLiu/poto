3ef81b53a80744011748a5e0c33391fde26836b9
Alex.Liu <larch.liu@gmail.com>
2022-11-06 19:46:31 +0800

    chore: header meta

--- apps/poto/vite.config.ts
+++ apps/poto/vite.config.ts
@@ -75,8 +75,8 @@ export default defineConfig({
       registerType: 'autoUpdate',
       includeAssets: ['favicon.svg', 'safari-pinned-tab.svg'],
       manifest: {
-        name: 'Vitesse',
-        short_name: 'Vitesse',
+        name: 'Poto',
+        short_name: 'Poto',
         theme_color: '#ffffff',
         icons: [
           {
